599ee3ddc6d0b9ad50ce6896a0d2819475b64d53
Justin Luther <justinluther@gmail.com>
2022-09-28 21:14:00 -0400

    refactoring and commenting.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,45 @@
+use reqwest::header::{CONTENT_TYPE, AUTHORIZATION};
 use std::env;
 use serde_json::{json, Value};
 
-use reqwest::header::{CONTENT_TYPE, AUTHORIZATION};
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+    // Build up the API request data
+    let post_data = build_post_payload();
+    let client = reqwest::Client::new();
+    let res = client.post("https://api.openai.com/v1/completions")
+        .header(CONTENT_TYPE, "application/json")
+        .header(AUTHORIZATION, build_auth_string())
+        .json(&post_data)
+        .send()
+        .await?;
+
+    // Receive the API response    
+    let res_body = res
+        .text()
+        .await?;
+    
+    // Parse the response into a serde json Value struct and get the value for 
+    // the "choices" key.
+    let v: Value = serde_json::from_str(&res_body)?;    
+    println!("{:#?}", v["choices"]);
+    Ok(())
+}
+
+fn build_auth_string() -> String {
     let api_key = env::var("OPEN_API_ACCESS_KEY").unwrap();
     let mut auth_string = String::from("Bearer ");
     auth_string.push_str(&api_key);
+    auth_string
+}
 
+fn build_post_payload() -> Value {
     let post_data = json!({
         "model": "text-davinci-002",
         "prompt": "Write a joke about racoons",
         "temperature": 0,
         "max_tokens": 500,
     });
-
-    let client = reqwest::Client::new();
-    let res = client.post("https://api.openai.com/v1/completions")
-        .header(CONTENT_TYPE, "application/json")
-        .header(AUTHORIZATION, auth_string)
-        .json(&post_data)
-        .send()
-        .await?;
-
-    let res_body = res
-        .text()
-        .await?;
-        
-    let v: Value = serde_json::from_str(&res_body)?;    
-    println!("{:#?}", v["choices"]);
-    Ok(())
+    post_data
 }
