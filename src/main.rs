b3a59a99d04547faaddb474324f6604ad64d3840
Justin Luther <justinluther@gmail.com>
2022-09-28 22:54:05 -0400

    works with files now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use reqwest::header::{CONTENT_TYPE, AUTHORIZATION};
-use std::env;
+use std::{env, fs, io::Write};
 use serde_json::{json, Value};
 
 #[tokio::main]
@@ -20,10 +20,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .text()
         .await?;
     
-    // Parse the response into a serde json Value struct and get the value for 
-    // the "choices" key.
-    let v: Value = serde_json::from_str(&res_body)?;    
-    println!("{:#?}", v["choices"]);
+    // Parse the response into a serde json Value struct and write the responses
+    // out to the file.
+    let v: Value = serde_json::from_str(&res_body)?;
+    println!("{:?}", v);
+    let divider = b"\n___________________________\n";
+    for choice in v["choices"].as_array().unwrap() {
+        let choice_obj = choice.as_object().unwrap();
+        let text = choice_obj["text"].as_str().unwrap();
+
+        let mut file = fs::OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open("prompt.txt")
+            .unwrap();
+        file.write_all(divider).unwrap();
+        file.write_all(text.as_bytes()).unwrap();
+    }
+
+    // Write out a final divider and then append the suffix.
+    let mut file = fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("prompt.txt")
+        .unwrap();
+    file.write_all(divider).unwrap();
+    let suffix = fs::read("suffix.txt").unwrap();
+    file.write_all(&suffix).unwrap();
     Ok(())
 }
 
@@ -35,11 +58,18 @@ fn build_auth_string() -> String {
 }
 
 fn build_post_payload() -> Value {
+    let prompt = fs::read_to_string("prompt.txt").unwrap();
+    let suffix = fs::read_to_string("suffix.txt").unwrap();
+    let used_chars = prompt.chars().count() + suffix.chars().count();
+    let used_tokens = 0.3 * used_chars as f32;
+    let max_tokens = 4093.0 - used_tokens;
     let post_data = json!({
         "model": "text-davinci-002",
-        "prompt": "Write a joke about racoons",
-        "temperature": 0,
-        "max_tokens": 500,
+        "prompt": prompt,
+        "suffix": suffix,
+        "temperature": 0.8,
+        "max_tokens": max_tokens as u32,
+        "n": 3,
     });
     post_data
 }
