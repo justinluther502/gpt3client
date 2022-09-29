b01464e12c60f08914f5bbe4fb5c35a0d29dc294
Justin Luther <justinluther@gmail.com>
2022-09-28 21:01:05 -0400

    working prototype

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
-use std::collections::HashMap;
+use std::env;
+use serde_json::{json, Value};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get("https://httpbin.org/ip")?
-        .json::<HashMap<String, String>>()?;
-    println!("{:#?}", resp);
+use reqwest::header::{CONTENT_TYPE, AUTHORIZATION};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let api_key = env::var("OPEN_API_ACCESS_KEY").unwrap();
+    let mut auth_string = String::from("Bearer ");
+    auth_string.push_str(&api_key);
+
+    let post_data = json!({
+        "model": "text-davinci-002",
+        "prompt": "Write a joke about racoons",
+        "temperature": 0,
+        "max_tokens": 500,
+    });
+
+    let client = reqwest::Client::new();
+    let res = client.post("https://api.openai.com/v1/completions")
+        .header(CONTENT_TYPE, "application/json")
+        .header(AUTHORIZATION, auth_string)
+        .json(&post_data)
+        .send()
+        .await?;
+
+    let res_body = res
+        .text()
+        .await?;
+        
+    let v: Value = serde_json::from_str(&res_body)?;    
+    println!("{:#?}", v["choices"]);
     Ok(())
 }
