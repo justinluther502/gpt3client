38d8a3813386dd76576c7d626bb0a591871732aa
Justin Luther <justinluther@gmail.com>
2022-10-05 20:55:17 -0400

    max token calcs correctly for other models.

--- src/main.rs
+++ src/main.rs
@@ -88,7 +88,13 @@ fn build_post_payload(user_cfg: &UserConfig, model_cfg: &ModelConfig) -> Value {
     let suffix = fs::read_to_string(&user_cfg.suffix_filename).unwrap();
     let used_chars = prompt.chars().count() + suffix.chars().count();
     let used_tokens = 0.3 * used_chars as f32;
-    let max_tokens = 4093 - used_tokens as u32;
+    
+    // Davinci-002 has a higher max token amount than other models.
+    let max_tokens = if model_cfg.model == "text-davinci-002" {
+        4093 - used_tokens as u32
+    } else {
+        2048 - used_tokens as u32
+    };
     
     // Return JSON, including suffix value based on model config value.
     if model_cfg.include_suffix {
