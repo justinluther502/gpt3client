d33d836f7fbce7b0260279913eb3dadfbd5cf3a9
Justin Luther <justinluther@gmail.com>
2022-10-05 21:04:17 -0400

    ran rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use reqwest::header::{CONTENT_TYPE, AUTHORIZATION};
-use std::{env, fs, io::Write};
-use serde_json::{json, Value};
+use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
 use serde_derive::Deserialize;
+use serde_json::{json, Value};
+use std::{env, fs, io::Write};
 
 #[derive(Deserialize)]
 struct Config {
@@ -14,14 +14,14 @@ struct ModelConfig {
     model: String,
     temperature: f32,
     choices: u32,
-    include_suffix: bool
+    include_suffix: bool,
 }
 
 #[derive(Deserialize)]
 struct UserConfig {
     api_key_env_var: String,
     prompt_filename: String,
-    suffix_filename: String
+    suffix_filename: String,
 }
 
 #[tokio::main]
@@ -29,24 +29,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Grab config from api_config.toml
     let config_contents = fs::read_to_string("api_config.toml")
         .expect("Could not read api_config.toml. Is it missing?");
-    let config: Config = toml::from_str(&config_contents)
-        .expect("Couldn't parse api_config.toml.");
-    
+    let config: Config = toml::from_str(&config_contents).expect("Couldn't parse api_config.toml.");
+
     // Build up the API request data
     let post_data = build_post_payload(&config.user, &config.model);
     let client = reqwest::Client::new();
-    let res = client.post("https://api.openai.com/v1/completions")
+    let res = client
+        .post("https://api.openai.com/v1/completions")
         .header(CONTENT_TYPE, "application/json")
-        .header(AUTHORIZATION, build_auth_string(&config.user.api_key_env_var))
+        .header(
+            AUTHORIZATION,
+            build_auth_string(&config.user.api_key_env_var),
+        )
         .json(&post_data)
         .send()
         .await?;
 
-    // Receive the API response    
-    let res_body = res
-        .text()
-        .await?;
-    
+    // Receive the API response
+    let res_body = res.text().await?;
+
     // Parse the response into a serde json Value struct and write the responses
     // out to the file.
     let v: Value = serde_json::from_str(&res_body)?;
@@ -88,14 +89,14 @@ fn build_post_payload(user_cfg: &UserConfig, model_cfg: &ModelConfig) -> Value {
     let suffix = fs::read_to_string(&user_cfg.suffix_filename).unwrap();
     let used_chars = prompt.chars().count() + suffix.chars().count();
     let used_tokens = 0.3 * used_chars as f32;
-    
+
     // Davinci-002 has a higher max token amount than other models.
     let max_tokens = if model_cfg.model == "text-davinci-002" {
         4093 - used_tokens as u32
     } else {
         2048 - used_tokens as u32
     };
-    
+
     // Return JSON, including suffix value based on model config value.
     if model_cfg.include_suffix {
         json!({
