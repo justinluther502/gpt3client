0688aa4b83d6fb81183f43bd12dba408366ec94a
Justin Luther <justinluther@gmail.com>
2022-10-07 20:55:01 -0400

    formatting

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,8 @@ mod text_writer;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = cfg_parser::parse_config();
-    let user = &config.user;
-    let model = &config.model;
-    let post_data = post_payload::post_content(&user, &model);
-    let res_body = api_request::send_request(&user.api_key_env_var, &post_data);
+    let post_data = post_payload::post_content(&config.user, &config.model);
+    let res_body = api_request::send_request(&config.user.api_key_env_var, &post_data);
     text_writer::write_resp_choices(
         &res_body,
         &config.user.prompt_filename,
